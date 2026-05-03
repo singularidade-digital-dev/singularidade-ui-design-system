31a2b304f9fa304c1b22ab29ae9f22672588109a
SINGULARIDADE.DIGITAL <singularidade.digital@singularidade.digital>
2026-05-03 20:02:01 -0300

    chore: add commitlint + lefthook hooks

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+};
